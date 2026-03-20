50cbc209ba43ec8350b5176e9c9d4bdf8dbe4b79
Elizabeth Heasley <SliceThePi@users.noreply.github.com>
2026-03-20 14:19:35 -0500

    Add mutators for Puzzle

--- src/nonogram/nonogram.ts
+++ src/nonogram/nonogram.ts
@@ -62,7 +62,7 @@ class Grid {
         if (typeof width_cells === 'number') {
             if (typeof height !== 'number')
                 throw new GridError('Invalid usage of Grid constructor!')
-            this.cells = new Array(height).map(() => new Array(width_cells).fill(null))
+            this.cells = new Array(height).fill(null).map(() => new Array(width_cells).fill(null))
         }
         else {
             if (height !== undefined)
@@ -198,14 +198,14 @@ class Grid {
 }
 
 class Puzzle {
-    column_rules: ReadonlyArray<Rule>
-    row_rules: ReadonlyArray<Rule>
+    #column_rules: Rule[]
+    #row_rules: Rule[]
     #input_grid: Grid
     #solved_grid: Grid | null | undefined
 
     constructor(column_rules: number[][], row_rules: number[][]) {
-        this.column_rules = column_rules.map(rule => new Rule(rule))
-        this.row_rules = row_rules.map(rule => new Rule(rule))
+        this.#column_rules = column_rules.map(rule => new Rule(rule))
+        this.#row_rules = row_rules.map(rule => new Rule(rule))
         this.#input_grid = new Grid(column_rules.length, row_rules.length)
     }
 
@@ -221,12 +221,96 @@ class Puzzle {
         return this.input_grid.height
     }
 
+    get column_rules() {
+        return this.#column_rules
+    }
+
+    get row_rules() {
+        return this.#row_rules
+    }
+
     get(x: number, y: number): Cell {
         return this.input_grid.get(x, y)
     }
 
     set(x: number, y: number, cell: Cell) {
         this.#input_grid.set(x, y, cell)
+    }
+
+    push_column(column?: [Rule, Cell[]]) {
+        if (column) {
+            const [rule, cells] = column
+            this.column_rules.push(rule)
+            this.#input_grid.push_column(cells)
+        }
+        else {
+            this.column_rules.push(new Rule([]))
+            this.#input_grid.push_column()
+        }
+        return this.width
+    }
+
+    pop_column(): [Rule, Cell[]] {
+        if (this.width <= 1)
+            throw new EmptyGridError()
+        return [this.column_rules.pop()!, this.#input_grid.pop_column()]
+    }
+
+    unshift_column(column?: [Rule, Cell[]]) {
+        if (column) {
+            const [rule, cells] = column
+            this.column_rules.unshift(rule)
+            this.#input_grid.unshift_column(cells)
+        }
+        else {
+            this.column_rules.unshift(new Rule([]))
+            this.#input_grid.unshift_column()
+        }
+        return this.width
+    }
+
+    shift_column(): [Rule, Cell[]] {
+        if (this.width <= 1)
+            throw new EmptyGridError()
+        return [this.column_rules.shift()!, this.#input_grid.shift_column()]
+    }
+
+    push_row(row?: [Rule, Cell[]]) {
+        if (row) {
+            const [rule, cells] = row
+            this.row_rules.push(rule)
+            this.#input_grid.push_row(cells)
+        }
+        else {
+            this.row_rules.push(new Rule([]))
+            this.#input_grid.push_row()
+        }
+        return this.height
+    }
+
+    pop_row(): [Rule, Cell[]] {
+        if (this.height <= 1)
+            throw new EmptyGridError()
+        return [this.row_rules.pop()!, this.#input_grid.pop_row()]
+    }
+
+    unshift_row(row?: [Rule, Cell[]]) {
+        if (row) {
+            const [rule, cells] = row
+            this.row_rules.unshift(rule)
+            this.#input_grid.unshift_row(cells)
+        }
+        else {
+            this.row_rules.unshift(new Rule([]))
+            this.#input_grid.unshift_row()
+        }
+        return this.height
+    }
+
+    shift_row(): [Rule, Cell[]] {
+        if (this.height <= 1)
+            throw new EmptyGridError()
+        return [this.row_rules.shift()!, this.#input_grid.shift_row()]
     }
 
     get solved_grid() {
